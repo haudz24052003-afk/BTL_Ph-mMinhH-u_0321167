6a19c47e513943ad3ba848c6ab56df0c51df6877
haudz24052003-afk <haudz24052003@gmail.com>
2025-09-29 18:02:12 +0700

    hoàn thiện UserControl_1

---
+++ BTL_UserControl/BTL_UserControl/UserControl1/BT1_UserControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BTL_UserControl.UserControl1
+{
+    /// <summary>
+    /// Interaction logic for BT1_UserControl.xaml
+    /// </summary>
+    public partial class BT1_UserControl : UserControl
+    {
+        public BT1_UserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
